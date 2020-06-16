b16b16bc6bc87e3501679526d70f37cb10de6fb4
TomazSouza <developer.software.ti@gmail.com>
2020-06-16 15:34:22 -0300

    Implementado reycler e layout na HomeFragment

---
+++ app/src/main/java/com/seletivo/promobit/application/BaseApp.kt
@@ -0,0 +1,11 @@
+package com.seletivo.promobit.application
+
+import android.app.Application
+
+class BaseApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/seletivo/promobit/db/Item.kt
@@ -0,0 +1,8 @@
+package com.seletivo.promobit.db
+
+open class Item {
+
+    var title: String? = ""
+    var subtitle: String? = ""
+
+}
